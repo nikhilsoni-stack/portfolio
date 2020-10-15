22dc2770667ed4be516d672424a54f5e774b0526
nikhilsoni-stack <aanikhilsoni@gmail.com>
2020-10-15 17:09:40 +0530

    Updates

---
+++ precache-manifest.37b30eb7e1942b17e373c2b3a806e48d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9fd801c09d6e76accba946e746ba280",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "024290fea88d553211d6",
+    "url": "/portfolio/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2d3b657a450e443967e2",
+    "url": "/portfolio/static/js/2.0785a73d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.0785a73d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024290fea88d553211d6",
+    "url": "/portfolio/static/js/main.20e35e53.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d655b2898ece0845dcad83d9b6758b55",
+    "url": "/portfolio/static/media/logo.d655b289.svg"
+  }
+]);
